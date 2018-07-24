55191671d9c15c6e3e0518970b0180acb6057dfb
dominikfiala <domo.fiala@gmail.com>
2018-07-24 22:25:30 +0200

    fix: public path pro github pages

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     index: path.resolve(__dirname, '../app/index.html'),
     assetsRoot: path.resolve(__dirname, '../app'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/kolik/app/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
